e94faf098ee5bb28a31b929ffeafaca836ddeba1
https-allan <code.allanjose@gmail.com>
2025-02-16 13:38:05 -0400

    update

--- clon3r.sh
+++ clon3r.sh
@@ -1,5 +1,18 @@
 #!/bin/zsh
 
+show_banner () {
+  echo ""
+  echo -e "\t\t\t███╗   ███╗ █████╗ ███████╗██╗ █████╗ ██████╗  ██████╗ ██╗   ██╗"
+  echo -e "\t\t\t████╗ ████║██╔══██╗██╔════╝██║██╔══██╗██╔══██╗██╔═══██╗╚██╗ ██╔╝"
+  echo -e "\t\t\t██╔████╔██║███████║█████╗  ██║███████║██████╔╝██║   ██║ ╚████╔╝ "
+  echo -e "\t\t\t██║╚██╔╝██║██╔══██║██╔══╝  ██║██╔══██║██╔══██╗██║   ██║  ╚██╔╝  "
+  echo -e "\t\t\t██║ ╚═╝ ██║██║  ██║██║     ██║██║  ██║██████╔╝╚██████╔╝   ██║   "
+  echo -e "\t\t\t╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝    ╚═╝   "
+  echo -e ""
+  echo -e "                            \t\tCreated by Mafiaboy0authx                      "
+}
+show_banner
+
 echo -n "\n Digite a URL do site: "
 read url 
 
@@ -9,7 +22,7 @@ read folder
 wget --mirror --page-requisites --convert-links --adjust-extension --no-parent -P "$folder" "$url"
 
 if [[ $? -eq 0 ]]; then
-  echo "\n✅ Clone Complete: Target site successfully cloned into folder \"$folder_name\".\n"
+  echo "\n ✅ Clone Complete: Target site successfully cloned into folder \"$folder_name\".\n"
 else
-  echo "\n❌ Clone Failed: Invalid URL or inaccessible target. Check the address and retry.\n"
+  echo "\n ❌ Clone Failed: Invalid URL or inaccessible target. Check the address and retry.\n"
 fi
